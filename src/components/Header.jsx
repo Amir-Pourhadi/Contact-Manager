f4ccf524ccb061b8280779fc9d17abf1ef9654bb
Amir Pourhadi <Alex.CE1379@Gmail.com>
2021-08-02 13:28:38 +0430

    h2 to h1

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ export default function Header() {
 	return (
 		<div className="ui fixed menu">
 			<div className="ui container center">
-				<h2>Contact Manager</h2>
+				<h1>Contact Manager</h1>
 			</div>
 		</div>
 	);
