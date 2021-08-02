b0d0fa8bf6307d321500e399c3a4e2e19eaffecc
Amir Pourhadi <Alex.CE1379@Gmail.com>
2021-08-03 03:20:50 +0430

    contacts rendered

--- src/components/ContactList.jsx
+++ src/components/ContactList.jsx
@@ -1,3 +1,17 @@
+import contacts from "../data/contacts.json";
+
 export default function ContactList() {
-	return <div className="ui celled list">Contact List</div>;
+	return (
+		<div className="ui celled list">
+			{contacts.map(({ id, name, email }) => (
+				<div className="item" key={id}>
+					<div className="contact">
+						<div className="header">{name}</div>
+						<div>{email}</div>
+					</div>
+					<i className="trash alternate outline icon"></i>
+				</div>
+			))}
+		</div>
+	);
 }
