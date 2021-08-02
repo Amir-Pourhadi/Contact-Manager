dcd58eb5f76a2bbe3e485b88176b3cf5ff5821f2
Amir Pourhadi <Alex.CE1379@Gmail.com>
2021-08-02 13:26:03 +0430

    component updated

--- src/components/AddContact.jsx
+++ src/components/AddContact.jsx
@@ -1,7 +1,18 @@
 export default function AddContact() {
 	return (
-		<div>
-			
+		<div className="ui main">
+			<h2>Add Contact</h2>
+			<form className="ui form">
+				<div className="field">
+					<label htmlFor="input-name">Name</label>
+					<input id="input-name" type="text" placeholder="Name" />
+				</div>
+				<div className="field">
+					<label htmlFor="input-email">Email</label>
+					<input id="input-email" type="text" placeholder="Email" />
+				</div>
+				<input type="submit" value="Add" className="ui button blue" />
+			</form>
 		</div>
-	)
+	);
 }
