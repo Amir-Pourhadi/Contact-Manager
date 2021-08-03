d41088be69a97ae2802746b656bb74ba6e98abc5
Amir Pourhadi <Alex.CE1379@Gmail.com>
2021-08-03 12:59:17 +0430

    render a sample contact

--- src/components/ContactDetail.jsx
+++ src/components/ContactDetail.jsx
@@ -1,9 +1,17 @@
-import user from '../images/user.jpg'
+import UserPhoto from "../images/user.jpg";
 
 export default function ContactDetail() {
 	return (
-		<div>
-			
+		<div className="main">
+			<div className="ui card centered">
+				<div className="image">
+					<img src={UserPhoto} alt="user" />
+				</div>
+				<div className="content">
+					<div className="header">Alex</div>
+					<div className="description">Alex.CE1379@Gmail.com</div>
+				</div>
+			</div>
 		</div>
-	)
+	);
 }
