9a8a326a8bd7f486faa4ac833122e4cadc7d582e
Amir Pourhadi <Alex.CE1379@Gmail.com>
2021-08-03 09:30:11 +0430

    Link added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,12 @@
+import { Link } from "react-router-dom";
+
 export default function Header() {
 	return (
 		<div className="ui fixed menu">
 			<div className="ui container center">
-				<h1>Contact Manager</h1>
+				<Link to="/">
+					<h1>Contact Manager</h1>
+				</Link>
 			</div>
 		</div>
 	);
