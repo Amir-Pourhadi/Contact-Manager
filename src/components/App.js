5d97209297c2ec98f716e1522715f2e01e1266e8
Amir Pourhadi <Alex.CE1379@Gmail.com>
2021-08-03 04:31:11 +0430

    AddContacts component removed

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,4 @@
 import "semantic-ui-css/semantic.min.css";
-import AddContact from "./AddContact";
 import "./App.css";
 import ContactList from "./ContactList";
 import Header from "./Header";
@@ -8,7 +7,6 @@ export default function App() {
 	return (
 		<div className="ui container">
 			<Header />
-			<AddContact />
 			<ContactList />
 		</div>
 	);
