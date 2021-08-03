57ab19484d9d15df68f191e762fb95b222fc3eab
Amir Pourhadi <Alex.CE1379@Gmail.com>
2021-08-03 04:31:45 +0430

    handleAdd contact

--- src/components/AddContact.jsx
+++ src/components/AddContact.jsx
@@ -1,15 +1,52 @@
-export default function AddContact() {
+import { useState } from "react";
+
+export default function AddContact({ handleAddContact }) {
+	const [contact, setContact] = useState({ id: "", name: "", email: "" });
+
+	/**
+	 * To handle input changes and update contact state
+	 * @param {event} e
+	 */
+	const handleChange = (e) => {
+		setContact({ ...contact, [e.target.name]: e.target.value });
+	};
+
+	/**
+	 * To handle submit
+	 * @param {event} e
+	 */
+	const addContact = (e) => {
+		e.preventDefault();
+		if (contact.name && contact.email) handleAddContact(contact);
+		else alert("All fields are mandatory!");
+		setContact({ id: "", name: "", email: "" });
+	};
+
 	return (
 		<div className="ui main">
 			<h2>Add Contact</h2>
-			<form className="ui form">
+			<form className="ui form" onSubmit={addContact}>
 				<div className="field">
 					<label htmlFor="input-name">Name</label>
-					<input id="input-name" type="text" placeholder="Name" />
+					<input
+						id="input-name"
+						type="text"
+						name="name"
+						placeholder="Name"
+						value={contact.name}
+						onChange={handleChange}
+					/>
 				</div>
 				<div className="field">
 					<label htmlFor="input-email">Email</label>
-					<input id="input-email" type="text" placeholder="Email" />
+					<input
+						id="input-email"
+						type="text"
+						name="email"
+						placeholder="Email"
+						value={contact.email}
+						onChange={handleChange}
+					/>
 				</div>
 				<input type="submit" value="Add" className="ui button blue" />
 			</form>
--- src/components/ContactList.jsx
+++ src/components/ContactList.jsx
@@ -1,12 +1,23 @@
-import contacts from "../data/contacts.json";
+import { useState } from "react";
+import fakeContacts from "../data/contacts.json";
+import AddContact from "./AddContact";
 import ContactCard from "./ContactCard";
 
 export default function ContactList() {
+	const [contacts, setContacts] = useState(fakeContacts);
+
+	const handleAddContact = (contact) => {
+		setContacts([contact, ...contacts]);
+	};
+
 	return (
-		<div className="ui celled list">
-			{contacts.map((contact) => (
-				<ContactCard contact={contact} />
-			))}
-		</div>
+		<>
+			<AddContact handleAddContact={handleAddContact} />
+			<div className="ui celled list">
+				{contacts.map((contact, index) => (
+					<ContactCard contact={contact} key={index} />
+				))}
+			</div>
+		</>
 	);
 }
