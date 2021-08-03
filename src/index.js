abf8e14a4e29c8e60c85e961fcefd3e354a85502
Amir Pourhadi <Alex.CE1379@Gmail.com>
2021-08-03 08:11:56 +0430

    wrap project with router-dom

--- src/index.js
+++ src/index.js
@@ -1,4 +1,10 @@
 import { render } from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import App from "./components/App";
 
-render(<App />, document.getElementById("root"));
+render(
+	<Router>
+		<App />
+	</Router>,
+	document.getElementById("root")
+);
