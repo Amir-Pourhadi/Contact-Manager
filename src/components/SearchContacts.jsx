bde6ead09bb6f0364d3135ac5023cc24c7caa8eb
Amir Pourhadi <Alex.CE1379@Gmail.com>
2021-08-04 20:40:24 +0430

    component created

---
+++ src/components/SearchContacts.jsx
@@ -0,0 +1,10 @@
+export default function () {
+	return (
+		<div className="ui search">
+			<div className="ui icon input">
+				<input type="text" className="prompt" placeholder="Search Contacts" />
+				<i className="search icon"></i>
+			</div>
+		</div>
+	);
+}
