41bff9dbfbcef9bd1031054ea2f815f532796aab
Amir Pourhadi <Alex.CE1379@Gmail.com>
2021-08-06 17:39:12 +0430

    footer updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,11 +2,26 @@ export default function Footer() {
 	return (
 		<footer>
 			<ul>
-				<li>Developed by Amir Pourhadi</li>
+				<li>
+					Developed by Amir Pourhadi
+					<a
+						href="https://www.linkedin.com/in/amirpourhadi"
+						target="/"
+						data-inverted=""
+						data-tooltip="View Profile on LinkedIn"
+						data-position="top left">
+						<i className="linkedin icon"></i>
+					</a>
+				</li>
 				<li>Copyright, 2021 &copy; All Rights Reserved.</li>
 				<li>
-					<a href="https://github.com/Amir-Pourhadi/Contact-Manager" target="/">
-						GitHub
+					Github
+					<a
+						href="https://github.com/Amir-Pourhadi/Contact-Manager"
+						target="/"
+						data-inverted=""
+						data-tooltip="View Source on GitHub"
+						data-position="top left">
 						<i className="github icon"></i>
 					</a>
 				</li>
