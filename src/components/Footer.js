3c6aabd5da48640c70976dc50fda7e9fb2512938
Amir Pourhadi <Alex.CE1379@Gmail.com>
2021-08-06 16:22:19 +0430

    footer created

---
+++ src/components/Footer.js
@@ -0,0 +1,16 @@
+export default function Footer() {
+	return (
+		<footer>
+			<ul>
+				<li>Developed by Amir Pourhadi</li>
+				<li>Copyright, 2021 &copy; All Rights Reserved.</li>
+				<li>
+					<a href="https://github.com/Amir-Pourhadi/Contact-Manager" target="/">
+						GitHub
+						<i className="github icon"></i>
+					</a>
+				</li>
+			</ul>
+		</footer>
+	);
+}
