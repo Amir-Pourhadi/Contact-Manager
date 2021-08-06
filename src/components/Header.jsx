82c83c06c52eac8e766fca94f02bbe09a05d812c
Amir Pourhadi <Alex.CE1379@Gmail.com>
2021-08-06 16:23:23 +0430

    semantic html rule

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,12 +2,12 @@ import { Link } from "react-router-dom";
 
 export default function Header() {
 	return (
-		<div className="ui fixed menu">
+		<header className="ui fixed menu">
 			<div className="ui container center">
 				<Link to="/" style={{ color: "black" }}>
 					<h1>Contact Manager</h1>
 				</Link>
 			</div>
-		</div>
+		</header>
 	);
 }
